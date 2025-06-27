a1a2b95b3bf728c2f05617a7831fe61a2edc4c6d
ReneDussault <rene.dussault.aubert@gmail.com>
2025-06-27 14:17:38 -0400

    restructure and rename homeworks folders

--- Practice - Section 6 - Modules and Libraries/my_module.py
+++
@@ -1,9 +0,0 @@
-# my_module.py
-def my_function():
-    print("Hello from my_module!")
-
-
-my_variable = "This is a variable from my_module."
-
-if __name__ == "__main__":
-    print("This code runs because my_module.py is the main program.")
